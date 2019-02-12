cbdacd3d95c355725d435e02d5bc881145847b8e
cjyabraham <cjyabraham@gmail.com>
2019-02-12 11:37:51 +0700

    try wraith now

--- .circleci/wraith-test.sh
+++ .circleci/wraith-test.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+echo 'export PATH=$PATH:$HOME/bin:$HOME/terminus/bin' >> $BASH_ENV
+echo 'export BRANCH=$(echo $CIRCLE_BRANCH | grep -v '"'"'^\(master\|[0-9]\+.x\)$'"'"')' >> $BASH_ENV
+echo 'export PR_ENV=${BRANCH:+pr-$BRANCH}' >> $BASH_ENV
+echo 'export CIRCLE_ENV=ci-$CIRCLE_BUILD_NUM' >> $BASH_ENV
+# If we are on a pull request
+if [[ $CIRCLE_BRANCH != "master" && -n ${CIRCLE_PULL_REQUEST+x} ]]
+then
+	# Then use a pr- branch/multidev
+	PR_NUMBER=${CIRCLE_PULL_REQUEST##*/}
+	PR_BRANCH="pr-${PR_NUMBER}"
+	echo "export DEFAULT_ENV=pr-${PR_NUMBER}" >> $BASH_ENV
+else
+	# otherwise make the branch name multidev friendly
+	if [[ $CIRCLE_BRANCH == "master" ]]
+	then
+		echo "export DEFAULT_ENV=dev" >> $BASH_ENV
+	else
+		echo 'export DEFAULT_ENV=$(echo ${PR_ENV:-$CIRCLE_ENV} | tr '"'"'[:upper:]'"'"' '"'"'[:lower:]'"'"' | sed '"'"'s/[^0-9a-z-]//g'"'"' | cut -c -11 | sed '"'"'s/-$//'"'"')' >> $BASH_ENV
+	fi
+fi
+echo 'export TERMINUS_ENV=${TERMINUS_ENV:-$DEFAULT_ENV}' >> $BASH_ENV
+source $BASH_ENV
+
 if [[ (${CIRCLE_BRANCH} != "master" && -z ${CIRCLE_PULL_REQUEST+x}) || (${CIRCLE_BRANCH} == "master" && -n ${CIRCLE_PULL_REQUEST+x}) ]];
 then
     echo -e "CircleCI will only run Wraith tests on Pantheon if on the master branch or creating a pull requests.\n"
@@ -20,15 +43,6 @@ echo
 
 # Exit immediately on errors
 set -ex
-
-# Clear site cache
-terminus -n env:clear-cache $TERMINUS_SITE.$TERMINUS_ENV
-
-# Wake the multidev environment before running tests
-terminus -n env:wake $TERMINUS_SITE.$TERMINUS_ENV
-
-# Ping wp-cli to start ssh with the app server
-terminus -n wp $TERMINUS_SITE.$TERMINUS_ENV -- cli version
 
 # Run the Wraith tests
 cd wraith
