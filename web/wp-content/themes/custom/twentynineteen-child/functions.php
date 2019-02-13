de3dd3f45cf0f3e0c847b88951bc13c7cd38c18d
cjyabraham <cjyabraham@gmail.com>
2019-02-13 14:50:50 +0700

    setup wp code sniffer correctly

--- web/wp-content/themes/custom/twentynineteen-child/functions.php
+++ web/wp-content/themes/custom/twentynineteen-child/functions.php
@@ -1,23 +1,25 @@
 <?php
 /**
  * Twenty Nineteen child theme functions and definitions
+ *
+ * @package WordPress
  */
- 
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
 /**
  * Include custom styles
  */
-function my_theme_enqueue_styles() 
-{
- 
-    $parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
- 
-    wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
-    wp_enqueue_style(
-        'child-style',
-        get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
-        wp_get_theme()->get('Version')
-    );
+function my_theme_enqueue_styles() {
+
+	$parent_style = 'parent-style'; // This is 'twentyfifteen-style' for the Twenty Fifteen theme.
+
+	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style(
+		'child-style',
+		get_stylesheet_directory_uri() . '/style.css',
+		array( $parent_style ),
+		wp_get_theme()->get( 'Version' )
+	);
 }
-?>
+
